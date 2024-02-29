cbc2ca601d211ab518f80418bb0ec5b61d05afe3
semir-hamidovic <semir.hamidovic@shetani.de>
2024-02-29 15:47:28 +0100

    Jetzt aber

--- src/app/quranMain/quranMainId/page.tsx
+++ src/app/quranMain/quranMainId/page.tsx
@@ -73,32 +73,32 @@ export default function QuranMain() {
 		func(url);
 	}, []);
 
-	useEffect(() => {
-		const handleKeyPress = (event: { key: string }) => {
-			console.log("event", event);
+	// useEffect(() => {
+	// 	const handleKeyPress = (event: { key: string }) => {
+	// 		console.log("event", event);
 
-			if (typeof window !== "undefined" && preloadedImages.length > 0) {
-				if (event.key === "ArrowLeft") {
-					setCurrentIndex(
-						currentIndex + 2 >= preloadedImages.length ? 0 : currentIndex + 2,
-					);
-				} else if (event.key === "ArrowRight") {
-					showPreviousImages();
-				}
-			}
-		};
+	// 		if (typeof window !== "undefined" && preloadedImages.length > 0) {
+	// 			if (event.key === "ArrowLeft") {
+	// 				setCurrentIndex(
+	// 					currentIndex + 2 >= preloadedImages.length ? 0 : currentIndex + 2,
+	// 				);
+	// 			} else if (event.key === "ArrowRight") {
+	// 				showPreviousImages();
+	// 			}
+	// 		}
+	// 	};
 
-		if (typeof window !== "undefined") {
-			window.addEventListener("keydown", handleKeyPress);
+	// 	if (typeof window !== "undefined") {
+	// 		window.addEventListener("keydown", handleKeyPress);
 
-			// Aufräumen, wenn die Komponente unmountet wird
-			return () => {
-				window.removeEventListener("keydown", handleKeyPress);
-			};
-		}
+	// 		// Aufräumen, wenn die Komponente unmountet wird
+	// 		return () => {
+	// 			window.removeEventListener("keydown", handleKeyPress);
+	// 		};
+	// 	}
 
-		// Aufräumen, wenn die Komponente unmountet wird
-	}, [currentIndex, preloadedImages]);
+	// 	// Aufräumen, wenn die Komponente unmountet wird
+	// }, [currentIndex, preloadedImages]);
 
 	const showNextImages = () => {
 		setCurrentIndex(
